1e43da256f6730d868f50803ad3edd9aa9e532ec
Christopher Miller <admin@mctherealm.net>
2018-10-16 15:38:28 -0400

    Finished oauth with github

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181012173951) do
+ActiveRecord::Schema.define(version: 20181016193129) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20181012173951) do
     t.datetime "created_at",                          null: false
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
+    t.string   "name"
     t.string   "provider"
     t.datetime "remember_created_at"
     t.datetime "reset_password_sent_at"
